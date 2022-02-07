a645a7cc5bf58cb76a0b671fbd6c6388413a9d30
rifqidewangga <rifdewangga@gmail.com>
2022-02-07 12:41:02 +0700

    refactor example using best practice

--- examples/main.py
+++ examples/main.py
@@ -1,29 +1,30 @@
 import sys
-from zephyr import Zephyr, InvalidSensorData, SensorNotSupported, compensated_reading, c_to_kelvin
+from zephyr import Zephyr
+import zephyr
 
 
 def main():
-    env_temperature: float = c_to_kelvin(20.0)
+    env_temperature: float = zephyr.c_to_kelvin(20.0)
     env_pressure: float = 1024.0  # hPa
 
     if len(sys.argv) == 3:
         env_pressure = float(sys.argv[1])
-        env_temperature = c_to_kelvin(float(sys.argv[2]))
+        env_temperature = zephyr.c_to_kelvin(float(sys.argv[2]))
 
     sensor = None
 
     try:
         sensor = Zephyr()
-    except SensorNotSupported as e:
+    except zephyr.SensorNotSupported as e:
         print(e.message)
 
     if sensor is not None:
         while True:
             try:
                 qs = sensor.read_average()
-                qx = compensated_reading(qs, env_temperature, env_pressure)
+                qx = zephyr.compensated_reading(qs, env_temperature, env_pressure)
                 print(f"{qx:2f}")
-            except InvalidSensorData as e:
+            except zephyr.InvalidSensorData as e:
                 print(e.message)
             except KeyboardInterrupt:
                 print("Program terminated")
